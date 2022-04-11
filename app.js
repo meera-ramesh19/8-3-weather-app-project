732dad6c393f30d013c866abedcde937cd5ef07a
meera-ramesh19 <meeraramesh19@outlook.com>
2022-04-10 23:08:52 -0400

    added and styled 3day forecast'

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const BASE_URL = 'https://wttr.in/';
-
+const iconImage = '';
+const iconAltText = '';
 /***
  *
  * @params (object) event
@@ -8,13 +9,15 @@ const BASE_URL = 'https://wttr.in/';
  */
 const getLocation = (event) => {
   event.preventDefault();
+
   let location = document.querySelector('#location');
   console.log(event.target);
 
   let city = location.value;
+  location.value = '';
   console.log(city);
   city = city[0].toUpperCase() + city.slice(1);
-  //url fpr the fetch
+
   const locationUrl = `https://wttr.in/${city}?format=j1`;
 
   //fetch API data
@@ -44,63 +47,90 @@ const fetchLocationWeather = async (url, city) => {
  *
  * @return
  */
+const getThreeDayForecast = (response) => {
+  const { weather } = response;
+
+  const today = weather[0].date;
+  const tomorrow = weather[1].date;
+  const dayafter = weather[2].date;
+  weather[0].avgtempF,weather[0].maxtempF,weather[0].mintempF
+  const daysOfWeek = ['Sunday ', 'Monday ', 'Tueday ', 'Wednesday ', 'Thursday ', 'Friday', 'Saturday'];
+  const dateNow = new Date();
+  let dateNum = dateNow.getDay();
+
+  today
+};
+/**
+ *
+ * @param(object) apiresponse object
+ *
+ * @return
+ */
 const renderWeatherData = (response, city) => {
-  //temp_c,temp_f
-  // location = document.querySelector('#location');
-  console.log(
-    'in weather data',
-    response,
-    response.current_condition[0].FeelsLikeC,
-    response.current_condition[0].FeelsLikeF,
-    response.current_condition[0].cloudcover,
-    response.current_condition[0].humidity,
-    response.current_condition[0].observation_time,
-    response.current_condition[0].uvIndex,
-    response.current_condition[0].weatherCode,
-    response.current_condition[0].weatherDesc[0].value,
-    response.current_condition[0].weatherIconUrl[0].value,
-    response.request[0].query,
-    response.nearest_area[0].areaName[0].value,
-    response.nearest_area[0].country[0].value,
-    response.weather[0].astronomy[0].sunrise,
-    response.weather[0].astronomy[0].sunset,
-    response.weather[0].hourly[0].DewPointC,
-    response.weather[0].hourly[0].DewPointF,
-    response.weather[0].hourly[0].chanceofrain,
-    response.weather[0].hourly[0].chanceofsunshine,
-    response.weather[0].hourly[0].chanceofsnow,
-    response.weather[0].maxtempC,
-    response.weather[0].maxtempF,
-    response.weather[0].mintempC,
-    response.weather[0].mintempF,
-    response.weather[0].avgtempC,
-    response.weather[0].avgtempF,
-    response.weather[0].date,
-    response.weather[0].totalSnow_cm
-  );
   const { current_condition, weather, nearest_area } = response;
   const { areaName, region, country } = nearest_area[0];
-  const weatherInfo = document.querySelector('.weather');
 
-  weatherInfo.innerHTML += `
-  <div class="response-data">                                                <img src=${current_condition[0].weatherIconUrl[0].value} alt = ${current_condition[0].weatherDesc[0].value} />
-  <h3> <strong>${city}</strong></h3>
-  <p><strong>Nearest Area : </strong>${areaName[0].value}</p>
-  <p> <strong>Region : </strong>${region[0].value} <p>
-  <p> <strong>Country : </strong>${country[0].value} <p>
-  <p><strong>Feels Like : </strong>  ${current_condition[0].FeelsLikeF}°F</p>
-  <p><strong>Chance of Sunshine : </strong>  ${weather[0].hourly[0].chanceofsunshine}</p>
-  <p><strong>Chance of Rain :  </strong> ${weather[0].hourly[0].chanceofrain}</p>
-  <p><strong>Chance of Sunshine : </strong> ${weather[0].hourly[0].chanceofsunshine}</p>
-  <p><strong>Chance of Snow :  </strong> ${weather[0].hourly[0].chanceofsnow}</p>
-       </div>`;
+  console.log(weather);
+
+  const chanceofrain = weather[0].hourly[0].chanceofrain;
+  const chanceofsunshine = weather[0].hourly[0].chanceofsunshine;
+  const chanceofsnow = weather[0].hourly[0].chanceofsnow;
+  const weatherInfo = document.querySelector('.location-data');
+  const image = document.createElement('img');
+
+  weatherInfo.innerHTML = '';
+
+  if (chanceofsunshine > 50) {
+    image.src = './assets/icons8-summer.gif';
+    image.alt = 'sun';
+  } else if (chanceofrain > 50) {
+    image.src = './assets/icons8-torrential-rain.gif';
+    image.alt = 'rain';
+  } else if (chanceofsnow > 50) {
+    image.src = './assets/icons8-light-snow.gif';
+    image.alt = 'snow';
+  }
+  image.style.textAlign = 'center';
+  weatherInfo.append(image);
+
+  // div.innerHTML += '<img src="'+img.src+'" />';
+  weatherInfo.innerHTML += `<br><strong>${city}</strong></h2><p><strong>Nearest Area:</strong>${areaName[0].value}</p><p> <strong>Region:</strong>${region[0].value}<p><p><strong>Country:</strong>${country[0].value}<p><p><strong>Feels Like:</strong>${current_condition[0].FeelsLikeF}°F</p><p><strong>Chance of Sunshine:</strong>${weather[0].hourly[0].chanceofsunshine}</p><p><strong>Chance of Rain:</strong>${weather[0].hourly[0].chanceofrain}</p><p><strong>Chance of Snow:</strong>${weather[0].hourly[0].chanceofsnow}</p>`;
+
+  getThreeDayForecast(response);
 };
 
 /****
- *@params(string) error message
+ *@params(event)
  */
+const temperatureConverter = (event) => {
+  let temperatureInput = document.querySelector('#temperature');
+  const result = document.querySelector('.result');
 
-const temperatureConverter = () => {};
+  const checkedButton = document.querySelector(
+    'input[name="convert"]:checked '
+  );
+
+  let checkedValue = checkedButton.value;
+  let temptoConvert = Number(temperatureInput.value);
+  console.log(typeof temptoConvert, temptoConvert);
+  result.innerHTMl = '';
+  temperatureInput.value = '';
+  let celsiusFahrenheitResult = 0;
+  let temperatureNotation = '';
+  // let errorMessage = false;
+
+  if (checkedValue === 'celsius') {
+    celsiusFahrenheitResult = (temptoConvert - 32) / 1.8;
+    temperatureNotation = 'C';
+  } else {
+    celsiusFahrenheitResult = temptoConvert * 1.8 + 32;
+    temperatureNotation = 'F';
+  }
+
+  result.innerHTML = `Temperature is:${celsiusFahrenheitResult.toFixed(
+    2
+  )}<sup>&deg;</sup>${temperatureNotation}`;
+};
 
 /****
  *@params(string) error message
@@ -108,6 +138,7 @@ const temperatureConverter = () => {};
 const renderError = (error) => {
   console.log(error);
 };
+
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
 
@@ -116,5 +147,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   submitLocation.addEventListener('click', getLocation);
 
-  temperatureConverter();
+  const tempForm = document.querySelector('#temperature-input');
+
+  tempForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+
+    temperatureConverter();
+  });
+  // const convertBtn = document.querySelector('#conversion-btn');
+
+  // convertBtn.addEventListener('click', temperatureConverter);
 });
